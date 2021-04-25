16caf9654e12debb948e5e15af4511c132487416
yljiang <Alvin@telenavsoftware.com>
2021-04-25 13:16:21 +0800

    'init'

---
+++ src/views/Axios.vue
@@ -0,0 +1,22 @@
+<template>123123</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import axios from '@/utils/axios'
+
+export default {
+    name: 'Axios',
+    setup() {
+        axios
+            .get('/user/XPoet')
+            .then((res) => {
+                console.log('res', res)
+            })
+            .catch((err) => {
+                console.log('err', err)
+            })
+    }
+}
+</script>
+
+<style lang="stylus" scoped></style>
---
+++ src/views/Vuex.vue
@@ -0,0 +1,9 @@
+<template></template>
+
+<script>
+export default {
+    name: 'Vuex'
+}
+</script>
+
+<style scoped></style>
